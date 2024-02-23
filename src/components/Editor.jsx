3af17274ce69651a37042220f49f7f84a137e1e0
Ajay Joshi <joshiajay311@gmail.com>
2024-02-23 18:37:12 +0530

    added expansion and collapse functionality

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,11 +3,15 @@ import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
-import "../index.css"
-
+import "../index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { useState } from "react";
 
 function Editor(props) {
+  const [open, setOpen] = useState(true);
+
   const { language, displayName, value, onChange } = props;
 
   function handleChange(editor, data, value) {
@@ -15,10 +19,14 @@ function Editor(props) {
   }
 
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className="editor-title">
         {displayName}
-        <button>O/C</button>
+        <button type="button"
+        className="expand-collapse-btn"
+        onClick={() => setOpen(!open)}>
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
@@ -28,7 +36,7 @@ function Editor(props) {
           lineWrapping: true,
           lint: true,
           mode: language,
-          theme: 'material',
+          theme: "material",
           lineNumbers: true,
         }}
       />
---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+const PREFIX = 'code-editor-';
+
+function useLocalStorage(key, initialValue) {
+  const prefixedkey = PREFIX + key;
+
+  const [value, setValue] = useState(() => {
+    const jsonValue = localStorage.getItem(prefixedkey)
+
+    if (jsonValue != null) return JSON.parse(jsonValue)
+
+    if (typeof initialValue === "function") {
+      return initialValue()
+    } else {
+      return initialValue
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem(prefixedkey, JSON.stringify(value))
+  }, [prefixedkey, value])
+   
+  return [value, setValue]
+}
+
+export default useLocalStorage
